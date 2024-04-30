f0fc67ea0fc152019e3d1e304008aa581d0f3877
OlSiv <sojmoscow@gmail.com>
2024-04-30 03:40:16 +0300

    30_04_2024_2

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from my_app.models import Worker   
 
-# Create your views here.
+
 def index_page(request):
+	all_workers = Worker.objects.all()   
+	print(all_workers)   
+
+	workers_filtered = Worker.objects.filter(salary=5000)   ### new
+	print(workers_filtered)   ### new
+	
 	return render(request, 'index.html')
+	
+	
+def about_page(request):
+	return render(request, 'about.html')
